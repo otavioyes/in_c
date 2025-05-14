df152f4e57516b727be7807fbcb501b68ab354f6
Otavio Correa <correaotavio0409@gmail.com>
2025-05-14 14:46:30 -0300

    generating_delay

---
+++ 3ndEmC/gerando_delay.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+
+printf("\t GERANDO DELAY NA EXECUÇÃO DO PROGRAMA\n");
+sleep(5);
+printf("ESPEROU 5 SEGUNDO E VOLTOU A RODAR NOVAMENTE\n");
+
+return 0;
+}
