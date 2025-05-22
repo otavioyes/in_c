856ab21990acfe660a82f7aa784ee4bd03ec58dc
Otavio Correa <correaotavio0409@gmail.com>
2025-05-22 16:01:19 -0300

    cast and base 2

---
+++ 5ndEmC/tipos_dados_cc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+float num1  =   3.14;
+int num2;
+
+num2 = (int)num1;
+
+printf("\tNumero original: %f\n", num1);
+printf("\tNumero convertido: %d\n", num2);
+
+int  num_binario = 0b10111011;
+
+printf("\tNumero em decimal: %d\n", num_binario);
+
+
+getchar();
+return 0;
+
+}
